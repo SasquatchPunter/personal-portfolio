921e76d272c5532fcf48dd16f40c1799cd07ef54
Jeremy Elliott <jodomoi64@yahoo.ca>
2022-10-20 15:16:51 -0600

    fix: change home Link in site header to SiteNavLink Site Nav menu didn't close when clicking the home Link from it

--- components/nav/HeaderNav.js
+++ components/nav/HeaderNav.js
@@ -1,15 +1,13 @@
-import Link from "next/link";
 import SiteNavButton from "./SiteNavButton";
+import SiteNavLink from "./SiteNavLink";
 
 export default function HeaderNav({}) {
   return (
     <nav className="w-full h-full flex justify-between items-center m-auto text-xl">
-      <Link href="/">
-        <a>
-          <span className="text-xl font-medium">J</span>eremy{" "}
-          <span className="text-xl font-medium">E</span>lliott
-        </a>
-      </Link>
+      <SiteNavLink href="/">
+        <span className="text-xl font-medium">J</span>eremy{" "}
+        <span className="text-xl font-medium">E</span>lliott
+      </SiteNavLink>
 
       <SiteNavButton />
     </nav>
